93dda7321d1701c9791707f888324c2458053c16
Arthur Galiev <arthur.galiev4@gmail.com>
2020-09-18 00:21:12 +0300

    add color inverting

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -3,52 +3,36 @@ package seamcarving
 import java.awt.Color
 import java.awt.image.BufferedImage
 import java.io.File
-import java.util.*
+import java.nio.file.Files
+import java.nio.file.Path
 import javax.imageio.ImageIO
 
 
-fun main() {
+fun main(args: Array<String>) {
 
-    val scanner = Scanner(System.`in`)
-    println("Enter rectangle width:")
-    val width = scanner.nextLine().toInt()
-    println("Enter rectangle height:")
-    val height = scanner.nextLine().toInt()
-    println("Enter output image name:")
-    val outputFileName = scanner.nextLine()
+    val inputFile = args[1]
+    val outputFile = args[3]
+
+    val positiveImage = ImageIO.read(Files.newInputStream(Path.of(inputFile)))
+
+    val width: Int = positiveImage.getWidth()
+    val height: Int = positiveImage.getHeight()
 
     // Constructs a BufferedImage of one of the predefined image types.
-    val bufferedImage = BufferedImage(width, height, BufferedImage.TYPE_INT_RGB)
+    val negativeImage = BufferedImage(width, height, BufferedImage.TYPE_INT_RGB)
 
-    // Create a graphics which can be used to draw into the buffered image
-    val g2d = bufferedImage.createGraphics()
-
-    // add black background
-    g2d.color = Color.black
-    g2d.fillRect(0, 0, width, height)
-
-    // paint main and second diagonals
-    g2d.color = Color.RED
-
-    if (width >= height) {
-        val k = width / height
-        for (i in 0..width step k) {
-            g2d.fillRect(i, i / k, k, 1)
-            g2d.fillRect(i, height - 1 - i / k, k, 1)
-        }
-    } else {
-        val k = height / width
-        for (i in 0..height step k) {
-            g2d.fillRect(i / k, i, 1, k)
-            g2d.fillRect(i / k, height - 1 - i / k, 1, k)
+    for (row in 0 until width) {
+        for (col in 0 until height) {
+            val rgba: Int = positiveImage.getRGB(row, col)
+            var color = Color(rgba, true)
+            color = Color(255 - color.red,
+                    255 - color.green,
+                    255 - color.blue)
+            negativeImage.setRGB(row, col, color.rgb)
         }
     }
 
-    // Disposes of this graphics context and releases any system resources that it is using.
-    g2d.dispose()
-
-    // Save as PNG
-    val file = File(outputFileName)
-    ImageIO.write(bufferedImage, "png", file)
+    val file = File(outputFile)
+    ImageIO.write(negativeImage, "bmp", file)
 
 }
